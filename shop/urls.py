ed22fdac31942aba1557b961db4529e6182e2c7c
HertzyTambwe <hertytambwe@gmail.com>
2025-02-12 11:48:49 +0200

    Creation du formulaire signup

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from accounts.views import signup
 from store.views import index, product_detail
 from shop import settings
 from django.conf.urls.static import static
@@ -7,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('signup/', signup, name="signup"),
     path('product/<str:slug>/', product_detail, name="product"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
