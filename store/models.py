b41e008eaa6fedc849616d74e0074a85a6ce1abc
HertzyTambwe <hertytambwe@gmail.com>
2025-02-18 18:05:01 +0200

    modification de la methode delete_cart

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone 
 from django.db import models
 from django.urls import reverse
 
@@ -53,3 +54,12 @@ class Cart(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    def delete(self, *args, **kwargs):
+        for order in self.orders.all():
+            order.ordered = True
+            order.ordered_date = timezone.now()
+            order.save()
+
+        self.orders.clear()
+        super().delete(*args, **kwargs)
--- store/views.py
+++ store/views.py
@@ -21,6 +21,7 @@ def add_to_cart(request, slug):
     product = get_object_or_404(Product, slug=slug)
     cart, _ = Cart.objects.get_or_create(user=user)
     order, created = Order.objects.get_or_create(user=user,
+                                                 ordered=False,
                                                  product=product)
     
     if created:
@@ -41,7 +42,6 @@ def cart(request):
 
 def delete_cart(request):
     if cart := request.user.cart:
-        cart.orders.all().delete()
         cart.delete()
 
     return redirect('index')
